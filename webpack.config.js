41a0dea11261ed1fddf9e4fa96b28c08d39dc3a0
Tiago de A Fernandes <tfernandes@mprj.mp.br>
2017-06-26 17:06:52 -0300

    adding file-loader support and aditional assets dir'

--- webpack.config.js
+++ webpack.config.js
@@ -3,88 +3,92 @@ const path = require('path');
 const autoprefixer = require('autoprefixer');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-let extractStyles = new ExtractTextPlugin('[name].css')
-let extractHtml = new ExtractTextPlugin('[name].html')
+let extractStyles = new ExtractTextPlugin('[name].css');
+let extractHtml = new ExtractTextPlugin('[name].html');
 
 let config = {
-  stats: {
-    assets: false,
-    colors: true,
-    version: false,
-    hash: true,
-    timings: true,
-    chunks: false,
-    chunkModules: false
-  },
-  entry: {
-    index: [
-      path.resolve(__dirname, 'templates/index.pug')
-    ],
-
-    'css/application': [
-      path.resolve(__dirname, 'assets/styles/application.scss')
-    ],
-
-    'js/application': [
-      path.resolve(__dirname, 'assets/js/application.js')
-    ]
-  },
-  output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: '[name].js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.pug$/,
-        loader: extractHtml.extract({
-          loader: ['html-loader', 'pug-html-loader?pretty&exports=false']
-        })
-      },
-      {
-        test: /\.scss$/,
-        loader: extractStyles.extract({
-          loader: [
-            {
-              loader: 'css-loader'
-            },
-            {
-              loader: 'postcss-loader',
-              options: {
-              plugins: (loader) => [
-
-              require('autoprefixer')(),
-              require('cssnano')()
-              ]
-              }
-            },
-            {
-              loader: 'sass-loader'
-            }
-          ]
-        })
-      }
-    ]
-  },
-  plugins: [
-    new webpack.LoaderOptionsPlugin({
-      minimize: false,
-      debug: true,
-      options: {
-        postcss: [
-          autoprefixer({
-            browsers: ['last 2 version', 'Explorer >= 10', 'Android >= 4']
-          })
+    stats: {
+        assets: false,
+        colors: true,
+        version: false,
+        hash: true,
+        timings: true,
+        chunks: false,
+        chunkModules: false
+    },
+    entry: {
+        index: [
+            path.resolve(__dirname, 'assets/templates/index.pug')
         ],
-        sassLoader: {
-          includePaths: [
-            path.resolve(__dirname, 'node_modules/sanitize.css/')
-          ]
-        }
-      }
-    }),
-    extractStyles,
-    extractHtml
-  ]
+
+        'css/application': [
+            path.resolve(__dirname, 'assets/styles/application.scss')
+        ],
+
+        'js/application': [
+            path.resolve(__dirname, 'assets/js/application.js')
+        ],
+
+    },
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: '[name].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.pug$/,
+                loader: extractHtml.extract({
+                    use: ['html-loader', 'pug-html-loader?pretty&exports=false']
+                })
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: ['file-loader']
+            },
+            {
+                test: /\.scss$/,
+                loader: extractStyles.extract({
+                    loader: [
+                        {
+                            loader: 'css-loader'
+                        },
+                        {
+                            loader: 'postcss-loader',
+                            options: {
+                                plugins: (loader) => [
+                                    require('autoprefixer')(),
+                                    require('cssnano')()
+                                ]
+                            }
+                        },
+                        {
+                            loader: 'sass-loader'
+                        }
+                    ]
+                })
+            }
+        ]
+    },
+    plugins: [
+        new webpack.LoaderOptionsPlugin({
+            minimize: false,
+            debug: true,
+            options: {
+                postcss: [
+                    autoprefixer({
+                        browsers: ['last 2 version', 'Explorer >= 10', 'Android >= 4']
+                    })
+                ],
+                sassLoader: {
+                    includePaths: [
+                        path.resolve(__dirname, 'node_modules/sanitize.css/')
+                    ]
+                }
+            }
+        }),
+        extractStyles,
+        extractHtml
+    ]
 }
 module.exports = config
