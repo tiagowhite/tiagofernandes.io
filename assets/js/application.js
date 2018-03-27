cc2c8cdc06d6f54507241907813c435e09fdbb77
Tiago Fernandes <tiagowhite@gmail.com>
2018-03-27 20:28:37 -0300

    parallax funcionando

--- assets/js/application.js
+++ assets/js/application.js
@@ -4,37 +4,29 @@ import baffle from 'baffle';
 import Parallax from 'parallax-js';
 
 function app() {
-	baffleRun();
-	parallaxRun();
-
+  baffleRun();
+  parallaxRun();
 }
 
 function parallaxRun() {
-	/*document.addEventListener("DOMContentLoaded", function() {
-			var scene = document.querySelector('.wrapper');
-			var i = new Parallax(scene, {
-				calibrateX: !0,
-				calibrateY: !0,
-				invertX: !0,
-				invertY: !0,
-				scalarX: 100,
-				scalarY: 100,
-				pointerEvents: !0
-			});
-			i.desktop ? i.invert(!0, !0) : i.invert(!1, !1)
-		});*/
-  let scene = document.getElementById('scene');
-  let parallaxInstance = new Parallax(scene, {
-    relativeInput: true
+  const i = new Parallax(scene, {
+    calibrateX: !0,
+    calibrateY: !0,
+    invertX: !0,
+    invertY: !0,
+    scalarX: 100,
+    scalarY: 100,
+    pointerEvents: !0
   });
-  parallaxInstance.friction(0.2, 0.2);
-	}
+  i.desktop ? i.invert(!0, !0) : i.invert(!1, !1)
+}
 
 
-	function baffleRun() {
-		let hello = baffle('.hello', {
-			characters: '"█▓▒░█▓▒░█▓▒░<>/"',
-			speed: 75
-		}).reveal(1000);
-	}
-	app();
+function baffleRun() {
+  const hello = baffle('.hello', {
+    characters: '"█▓▒░█▓▒░█▓▒░<>/"',
+    speed: 75
+  }).reveal(1000);
+}
+
+app();
