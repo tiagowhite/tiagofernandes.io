921e2cb91467fd091cf59a49aa463bd9683a7997
Tiago Fernandes <tiagowhite@gmail.com>
2018-04-12 22:23:13 -0300

    post processing config and discover

--- assets/js/application.js
+++ assets/js/application.js
@@ -2,10 +2,12 @@
 import 'core-js';
 import baffle from 'baffle';
 import Parallax from 'parallax-js';
+import * as THREE from 'three';
 
 function app() {
   baffleRun();
   /*parallaxRun();*/
+  runThree();
 }
 
 function parallaxRun() {
@@ -27,6 +29,36 @@ function baffleRun() {
     characters: '"█▓▒░█▓▒░█▓▒░<>/"',
     speed: 75
   }).reveal(1000);
+}
+
+function runThree() {
+  const renderer = new THREE.WebGLRenderer({canvas: document.getElementById('myCanvas'), antialias: true});
+  /*renderer.setClearColor(0x1f0d23);*/
+  renderer.setClearColor(0xffffff);
+
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+  const camera = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 0.1, 3000);
+  const scene = new THREE.Scene();
+  const light = new THREE.AmbientLight(0xffffff, 0.5);
+
+  scene.add(light);
+
+  const geometry = new THREE.PlaneGeometry(200, 129, 0);
+  const texture = new THREE.TextureLoader().load('../img/logo0a.png');
+  const material = new THREE.MeshBasicMaterial({map: texture});
+  const mesh = new THREE.Mesh(geometry, material);
+
+  scene.add(mesh);
+
+  requestAnimationFrame(render);
+
+  function render() {
+
+    renderer.render(scene, camera);
+    requestAnimationFrame(render);
+  }
 }
 
 app();
